8ae1247b54661edef33df798717ed1aedb74a329
rheber <rheber@rediffmail.com>
2015-07-03 13:47:42 +1000

    read and output file

--- decomment.d
+++ decomment.d
@@ -2,8 +2,21 @@
 Main function.
 */
 
+import std.file;
+import std.stdio;
+
 void main(string[] args) {
+
   if(args.length < 2) {
     throw new Exception("Too few arguments.");
   }
+
+  File f = File(args[1], "r");
+  FILE* source = f.getFP();
+
+  while(!f.eof()) {
+    int c = getc(source);
+    putchar(c);
+  }
+
 }
