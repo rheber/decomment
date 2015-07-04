16b7466f9c7f31adfc8a0c39415e2d3b4fb5f486
rheber <rheber@rediffmail.com>
2015-07-04 17:49:29 +1000

    read json

--- decomment.d
+++ decomment.d
@@ -4,10 +4,11 @@ Main function.
 
 import std.exception;
 import std.file;
+import std.json;
 import std.stdio;
 
 void main(string[] args) {
-
+/*
   enforce(args.length > 1, "Too few arguments.");
 
   File f = File(args[1], "r");
@@ -17,5 +18,8 @@ void main(string[] args) {
     int c = getc(source);
     putchar(c);
   }
+*/
 
+  JSONValue j = parseJSON(readText("language.json"));
+  writeln(j);
 }
