80082b070ef72eed89c55ca77e641b298515e14f
rheber <rheber@rediffmail.com>
2015-07-07 15:43:14 +1000

    process c quotes

--- decomment.d
+++ decomment.d
@@ -30,12 +30,41 @@ main(string[] args) {
 
 }
 
+/*
+Check if the given character starts a quote.
+*/
 bool
 startOfQuote(FILE* source, in JSONValue[string] language, in int c) {
-  return true;
+  foreach(JSONValue j; language["quotes"].array()) {
+    if(c ==  j.str()[0]) { // Works if quote sequences are one character long.
+      return true;
+    }
+  }
+  return false;
 }
 
+/*
+Print to the end of a quote.
+Assumes escape and quote sequences are one character long and
+quotes begin and end with the same character.
+*/
 void
-outputQuote(FILE* source, in JSONValue[string] language, in int c) {
-  putchar(c);
+outputQuote(FILE* source, in JSONValue[string] language, in int start) {
+  bool escaped = false;
+
+  putchar(start);
+  while(true) {
+    int c = getc(source);
+    if(c == _F_EOF) {
+      return;
+    }
+    putchar(c);
+    if(c == language["escape"].str()[0]) {
+      escaped = true;
+    } else if(c == start && !escaped) {
+      return;
+    } else {
+      escaped = false;
+    }
+  }
 }
