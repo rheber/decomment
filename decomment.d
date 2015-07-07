a83d94581b8bec3850182a0e7c02011ef44e7167
rheber <rheber@rediffmail.com>
2015-07-07 15:17:44 +1000

    stubs

--- decomment.d
+++ decomment.d
@@ -7,19 +7,35 @@ import std.file;
 import std.json;
 import std.stdio;
 
-void main(string[] args) {
-/*
+void
+main(string[] args) {
+
   enforce(args.length > 1, "Too few arguments.");
 
   File f = File(args[1], "r");
   FILE* source = f.getFP();
 
+  JSONValue j = parseJSON(readText("language.json"));
+  // TODO: Pick language based on extension.
+  JSONValue[string] language = j["clang"].object();
+
   while(!f.eof()) {
     int c = getc(source);
-    putchar(c);
+    if(startOfQuote(source, language, c)) {
+      outputQuote(source, language, c);
+    } else {
+      putchar(c);
+    }
   }
-*/
 
-  JSONValue j = parseJSON(readText("language.json"));
-  writeln(j);
+}
+
+bool
+startOfQuote(FILE* source, in JSONValue[string] language, in int c) {
+  return true;
+}
+
+void
+outputQuote(FILE* source, in JSONValue[string] language, in int c) {
+  putchar(c);
 }
