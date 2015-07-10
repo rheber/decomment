a34b377b882dc1ba45b409694aedc16674b745b1
rheber <rheber@rediffmail.com>
2015-07-10 21:23:05 +1000

    test/in.py

---
+++ test/in.py
@@ -0,0 +1,17 @@
+#python test
+
+# test quotes
+a = 'a'
+b = "b"
+c = '''c'''
+d = """d"""
+
+# test escapes
+a = '\'ahoy!\''
+b = "\"ahoy!\""
+c = '''\nahoy!'''
+d = """\nahoy!"""
+
+# test long quotes
+a = ''''ahoy!'\n'''
+b = """"ahoy!"\n"""
