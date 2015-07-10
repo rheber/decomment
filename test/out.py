5778381fa872e4b58172cb271831636fc3257866
rheber <rheber@rediffmail.com>
2015-07-10 21:26:58 +1000

    test/out.py

--- test/out.py
+++ test/out.py
@@ -1,17 +1,17 @@
-#python test
 
-# test quotes
+
+
 a = 'a'
 b = "b"
 c = '''c'''
 d = """d"""
 
-# test escapes
+
 a = '\'ahoy!\''
 b = "\"ahoy!\""
 c = '''\nahoy!'''
 d = """\nahoy!"""
 
-# test long quotes
+
 a = '''bob's your uncle'''
 b = """bob"s your uncle"""
