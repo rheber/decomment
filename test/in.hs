2c19e97bad417eee4d6e0602e27e7015f2da2c26
rheber <rheber@rediffmail.com>
2015-11-12 14:49:35 +1100

    haskell data

---
+++ test/in.hs
@@ -0,0 +1,17 @@
+--haskell test
+
+-- quotes
+a = '"'
+b = "'"
+c = '\''
+d = "\"'
+e = '\\'
+f = "\\"
+
+{- This is a {-nesting-} comment. -}
+
+-- quoting comment sequences
+a' = "--"
+b' = "{-"
+
+--end of test
